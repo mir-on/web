d30b1d7821480bb401758b812f52b48f8e4f13f0
user <user@git.com>
2018-05-30 15:34:58 +0300

    hello & init for gunicorn

--- hello.py
+++ hello.py
@@ -6,12 +6,14 @@ bind = '0.0.0.0:8080'
 
 def application(environ, start_response):
     status = '200 OK'
-    raw_uri = environ['RAW_URI']
+    #raw_uri = environ['RAW_URI']
+    qstr = environ['QUERY_STRING'].split('&')
     try:
-        s = raw_uri[raw_uri.index('?')+1:].split('&')
-        print(s)
+        #s = raw_uri[raw_uri.index('?')+1:].split('&')
+        #print(s)
+        body = [ bytes(i + '\n', 'ascii') for i in qstr ]
         headers = [ ('Content-Type', 'text/plain') ]
         start_response(status, headers)
-        return [bytes('\n'.join(s).encode('utf-8'))]
-    except:
-        return [bytes('error', 'utf-8')]
+        return body
+    except BaseException as err:
+        return [bytes(err.__str__(), 'ascii')]
--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn stop
+sudo gunicorn3 -c hello.py hello:application
+
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
