7bfd2a0bffb339f4688c27e2085a6fb27bba2508
mir <mir@prog-mir.in.nevos-spb.ru>
2018-06-02 20:50:17 +0300

    author: ForeignKey

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -21,7 +21,7 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(django.contrib.auth.models.User)
     likes = models.ManyToManyField(django.contrib.auth.models.User)
 
     def __str__(self):
@@ -32,4 +32,4 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField('date published')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(django.contrib.auth.models.User)
